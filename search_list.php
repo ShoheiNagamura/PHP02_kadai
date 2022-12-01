aaba9fdd6ca15fa592162b3d741809874f7053a6
shouhei <shohei.nagamura@gmail.com>
2022-12-02 04:11:25 +0900

    フォーム画面のUIを整えた

--- search_list.php
+++ search_list.php
@@ -59,7 +59,7 @@ foreach ($result as $record) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/search_list.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>PHP課題02</title>
 </head>
 
